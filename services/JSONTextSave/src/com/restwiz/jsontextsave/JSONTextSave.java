c7f2bb0be2e4da2dfb37c630b18bbfffe5fd1cd6
Najira G <najira@medicalwizard.com.au>
2018-11-08 10:52:54 +0000

    New files added - [JSONTextSave] Files modified - [CWmwSQL,pom.xml,project-services.xml] New files added - [JSONTextSave] Files modified - [CWmwSQL,pom.xml,project-services.xml]

---
+++ services/JSONTextSave/src/com/restwiz/jsontextsave/JSONTextSave.java
@@ -0,0 +1,55 @@
+/*Copyright (c) 2015-2016 medicalwizard.com.au All Rights Reserved.
+ This software is the confidential and proprietary information of medicalwizard.com.au You shall not disclose such Confidential Information and shall use it only in accordance
+ with the terms of the source code license agreement you entered into with medicalwizard.com.au*/
+package com.restwiz.jsontextsave;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+
+import com.wavemaker.runtime.security.SecurityService;
+import com.wavemaker.runtime.service.annotations.ExposeToClient;
+import com.wavemaker.runtime.service.annotations.HideFromClient;
+
+import com.restwiz.cwmwsql.service.CWmwSQLQueryExecutorService;
+
+//import com.restwiz.jsontextsave.model.*;
+
+/**
+ * This is a singleton class with all its public methods exposed as REST APIs via generated controller class.
+ * To avoid exposing an API for a particular public method, annotate it with @HideFromClient.
+ *
+ * Method names will play a major role in defining the Http Method for the generated APIs. For example, a method name
+ * that starts with delete/remove, will make the API exposed as Http Method "DELETE".
+ *
+ * Method Parameters of type primitives (including java.lang.String) will be exposed as Query Parameters &
+ * Complex Types/Objects will become part of the Request body in the generated API.
+ */
+@ExposeToClient
+public class JSONTextSave {
+
+    private static final Logger logger = LoggerFactory.getLogger(JSONTextSave.class);
+
+    @Autowired
+    private SecurityService securityService;
+    
+    @Autowired
+    private CWmwSQLQueryExecutorService cWmwSQLQueryExecutorService;
+    
+    
+    public String saveText(String text){
+        String result = "";
+        
+        
+        
+        return result;
+        
+    }
+
+
+
+}
