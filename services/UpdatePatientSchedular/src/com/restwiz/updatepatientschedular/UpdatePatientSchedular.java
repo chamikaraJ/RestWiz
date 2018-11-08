57fb57fdb6589d0ca3196fd5a5ef74154e5521e9
Najira G <najira@medicalwizard.com.au>
2018-11-08 12:10:40 +0000

    New files added - [UpdatePatientSchedular] Files modified - [JSONTextSave,pom.xml,project-services.xml] New files added - [UpdatePatientSchedular] Files modified - [JSONTextSave,pom.xml,project-services.xml]

--- services/UpdatePatientSchedular/src/com/restwiz/updatepatientschedular/UpdatePatientSchedular.java
+++ services/UpdatePatientSchedular/src/com/restwiz/updatepatientschedular/UpdatePatientSchedular.java
@@ -1,53 +1,44 @@
 /*Copyright (c) 2015-2016 medicalwizard.com.au All Rights Reserved.
  This software is the confidential and proprietary information of medicalwizard.com.au You shall not disclose such Confidential Information and shall use it only in accordance
  with the terms of the source code license agreement you entered into with medicalwizard.com.au*/
-package com.restwiz.jsontextsave;
+package com.restwiz.updatepatientschedular;
 
-import com.restwiz.cwmwsql.JsontextTemp;
-import com.restwiz.cwmwsql.service.CWmwSQLQueryExecutorService;
-import com.restwiz.cwmwsql.service.JsontextTempService;
-import com.wavemaker.runtime.security.SecurityService;
-import com.wavemaker.runtime.service.annotations.ExposeToClient;
+import javax.servlet.http.HttpServletRequest;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
 import org.springframework.beans.factory.annotation.Autowired;
 
-//import com.restwiz.jsontextsave.model.*;
+
+import com.wavemaker.runtime.security.SecurityService;
+import com.wavemaker.runtime.service.annotations.ExposeToClient;
+import com.wavemaker.runtime.service.annotations.HideFromClient;
+
+//import com.restwiz.updatepatientschedular.model.*;
 
 /**
  * This is a singleton class with all its public methods exposed as REST APIs via generated controller class.
  * To avoid exposing an API for a particular public method, annotate it with @HideFromClient.
- * <p>
+ *
  * Method names will play a major role in defining the Http Method for the generated APIs. For example, a method name
  * that starts with delete/remove, will make the API exposed as Http Method "DELETE".
- * <p>
+ *
  * Method Parameters of type primitives (including java.lang.String) will be exposed as Query Parameters &
  * Complex Types/Objects will become part of the Request body in the generated API.
  */
 @ExposeToClient
-public class JSONTextSave {
+public class UpdatePatientSchedular {
 
-    private static final Logger logger = LoggerFactory.getLogger(JSONTextSave.class);
+    private static final Logger logger = LoggerFactory.getLogger(UpdatePatientSchedular.class);
 
     @Autowired
     private SecurityService securityService;
 
-    @Autowired
-    private JsontextTempService tempService;
-
-    @Autowired
-    private CWmwSQLQueryExecutorService cWmwSQLQueryExecutorService;
-
-
-    public JsontextTemp saveText(String text) {
+    public String updatePatient(){
         String result = "";
-
-        JsontextTemp temp = new JsontextTemp();
-        temp.setJsonText(text);
-        temp = tempService.create(temp);
-        return temp;
-
+        
+        return result;
     }
-
 
 }
